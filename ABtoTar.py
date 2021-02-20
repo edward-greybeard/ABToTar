0e24c8bca44f4f5e061064a96a8af29f6cec8396
edward-greybeard <ed.greybeard@gmail.com>
2021-02-20 14:22:14 +0000

    First pass of code done. Tested OK.

--- ABtoTar.py
+++ ABtoTar.py
@@ -2,6 +2,7 @@ import tarfile
 import os.path
 import logging
 import argparse
+from sys import exit
 
 AB_HEADER = b"ANDROID BACKUP"
 TAR_HEADER = b"\x1f\x8b\x08\x00\x00\x00\x00\x00"
@@ -21,21 +22,51 @@ def extract_tar_from_ab(path_to_ab, output_dir=None):
     if output_dir is None:
         output_dir = os.path.dirname(path_to_ab)
 
+    # Check we can open the file
     try:
         ab_data = open(path_to_ab, 'rb')
-    except FileNotFoundError:
+    except:
         logging.critical(f"Unable to open AB file at {path_to_ab}")
         return False
 
-    ab_data.seek(8)
+    # Check the AB file header is intact
+    ab_bytes_to_remove = ab_data.read(24)
 
+    if ab_bytes_to_remove[:14] == AB_HEADER:
+        logging.info("AB Header checked and intact")
+    else:
+        logging.error("AB Header not found; is it definitely the right file?")
+        return False
 
+    # Open the target tar file
+    output_path = build_tar_filepath(input_path, output_dir)
+
+    try:
+        output_file = open(output_path, 'wb')
+    except:
+        logging.error("Unable to open file at {output_path}")
+        return False
+
+    logging.info("Writing tar header..")
+    output_file.write(TAR_HEADER)
+
+    logging.info("Writing rest of AB file..")
+    output_file.write(ab_data.read())
+
+    logging.info("..done.")
+    logging.info("Closing files..")
+
+    output_file.close()
+    ab_data.close()
+    return True
 
 
 def build_tar_filepath(input_path, output_dir):
     input_filename = os.path.splitext(os.path.basename(input_path))[0]
-
-
+    output_filename = f"{input_filename}.tar.gz"
+    logging.info(f"Output filename: {output_filename}")
+    output_filepath = os.path.join(output_dir, output_filename)
+    return output_filepath
 
 
 if __name__ == '__main__':
@@ -43,6 +74,10 @@ if __name__ == '__main__':
     logging.info("AB to Tar")
     logging.info("By Greybeard")
     logging.info("-------------------")
+    logging.info("Global variables:")
+    logging.info(f"AB_HEADER:        {AB_HEADER}")
+    logging.info(f"TAR_HEADER:       {TAR_HEADER}")
+    logging.info(f"BYTES TO IGNORE:  {IGNORE_OFFSET}")
 
     parser = argparse.ArgumentParser(description="Convert an Android Backup (AB) file to a tar")
     parser.add_argument('-i', '--input', dest='input_path', type=str, required=True, action='store',
@@ -55,4 +90,15 @@ if __name__ == '__main__':
     input_path = args.input_path
     output_dir = args.output_dir
 
+    if os.path.exists(input_path) is not True:
+        logging.critical(f"{input_path} not found. Exiting..")
+        exit(1)
+
     logging.info(f"Input file: {input_path}")
+
+    if extract_tar_from_ab(input_path, output_dir) is True:
+        logging.info("Processing complete.")
+    else:
+        logging.info("Error with processing. Exiting..")
+
+
